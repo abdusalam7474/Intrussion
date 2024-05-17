fdb719bd0e4a85fca89b0b61b66fefca7ce543a8
abdusalam7474 <mideola824@gmail.com>
2024-05-17 23:31:25 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -123,8 +123,8 @@ with col2:
 predict_button = st.button("Predict")
 
 if predict_button:
-    predicted_category, data_df = predict_intrusion(user_input, selected_model)
+    #predicted_category, data_df = predict_intrusion(user_input, selected_model)
     st.subheader("Prediction Results")
     st.dataframe(data_df)
-    st.write("Predicted Category:", predicted_category)
+    #st.write("Predicted Category:", predicted_category)
 
